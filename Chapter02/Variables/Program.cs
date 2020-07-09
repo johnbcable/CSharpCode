72a04db2fddf26104d62fc90f16c68ae5303b4a5
John Cable <JohnCable@Warwickshire.gov.uk>
2020-07-09 18:39:15 +0100

    Further additions to Chapter02 code

---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object height = 1.88;       // storing a double in an object
+            object name = "Amir";       // storing a string in an object
+
+            Console.WriteLine($"{name} is {height} metres tall");
+
+            // int length1 = name.Length;                  // should give compile error
+            int length2 = ((string)name).Length;        // tell compiler it is a string
+
+            Console.WriteLine($"{name} has {length2} characters");
+
+        }
+    }
+}
