525c66cc05b2d11032197facb0b92315e626d5d5
John Cable <JohnCable@Warwickshire.gov.uk>
2020-07-13 08:37:27 +0100

    Tidying up of Chapter 2 plus more on the Exercise

--- Chapter02/Exercise02/Program.cs
+++ Chapter02/Exercise02/Program.cs
@@ -7,21 +7,21 @@ namespace Exercise02
     {
         static void Main(string[] args)
         {
-            WriteLine("------------------------------------------------------------");
-            WriteLine("Type\tBytes of Memory          Min                     Max");
-            WriteLine("------------------------------------------------------------");
-            WriteLine($"sbyte\t{sizeof(sbyte)}\t{sbyte.MinValue,20:N0}\t{sbyte.MaxValue,20:N0}");
-            WriteLine($"byte\t{sizeof(byte)}\t{byte.MinValue,20:N0}\t{byte.MaxValue,20:N0}");
-            WriteLine($"short\t{sizeof(short)}\t{short.MinValue,20:N0}\t{short.MaxValue,20:N0}");
-            WriteLine($"ushort\t{sizeof(ushort)}\t{ushort.MinValue,20:N0}\t{ushort.MaxValue,20:N0}");
-            WriteLine($"int\t{sizeof(int)}\t{int.MinValue,20:N0}\t{int.MaxValue,20:N0}");
-            WriteLine($"uint\t{sizeof(uint)}\t{uint.MinValue,20:N0}\t{uint.MaxValue,20:N0}");
-            WriteLine($"long\t{sizeof(long)}\t{long.MinValue,20:N0}\t{long.MaxValue,20:N0}");
-            WriteLine($"ulong\t{sizeof(ulong)}\t{ulong.MinValue,20:N0}\t{ulong.MaxValue,20:N0}");
-            WriteLine($"float\t{sizeof(float)}\t{float.MinValue,20:N0}\t{float.MaxValue,20:N0}");
-            WriteLine($"double\t{sizeof(double)}\t{double.MinValue,20:N0}\t{double.MaxValue,20:N0}");
-            WriteLine($"decimal\t{sizeof(decimal)}\t{decimal.MinValue,20:N0}\t{decimal.MaxValue,20:N0}");
-            WriteLine("------------------------------------------------------------");
+            WriteLine("----------------------------------------------------------------------");
+            WriteLine("Type\tBytes of Memory                    Min                               Max");
+            WriteLine("----------------------------------------------------------------------");
+            WriteLine($"sbyte\t{sizeof(sbyte)}\t{sbyte.MinValue,30:F0}\t{sbyte.MaxValue,30:F0}");
+            WriteLine($"byte\t{sizeof(byte)}\t{byte.MinValue,30:F0}\t{byte.MaxValue,30:F0}");
+            WriteLine($"short\t{sizeof(short)}\t{short.MinValue,30:F0}\t{short.MaxValue,30:F0}");
+            WriteLine($"ushort\t{sizeof(ushort)}\t{ushort.MinValue,30:F0}\t{ushort.MaxValue,30:F0}");
+            WriteLine($"int\t{sizeof(int)}\t{int.MinValue,30:F0}\t{int.MaxValue,30:F0}");
+            WriteLine($"uint\t{sizeof(uint)}\t{uint.MinValue,30:F0}\t{uint.MaxValue,30:F0}");
+            WriteLine($"long\t{sizeof(long)}\t{long.MinValue,30:F0}\t{long.MaxValue,30:F0}");
+            WriteLine($"ulong\t{sizeof(ulong)}\t{ulong.MinValue,30:F0}\t{ulong.MaxValue,30:F0}");
+            WriteLine($"float\t{sizeof(float)}\t{string.Format("{0:0.##############E+00}", float.MinValue)}\t{float.MaxValue,30:F0}");
+            WriteLine($"double\t{sizeof(double)}\t{double.MinValue,30:F0}\t{double.MaxValue,30:F0}");
+            WriteLine($"decimal\t{sizeof(decimal)}\t{decimal.MinValue,30:F0}\t{decimal.MaxValue,30:F0}");
+            WriteLine("----------------------------------------------------------------------");
          }
     }
 }
