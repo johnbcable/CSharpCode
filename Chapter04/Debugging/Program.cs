6f62a026a8d07c38044353baad5539e638f557e9
John Cable <JohnCable@Warwickshire.gov.uk>
2020-07-21 19:21:20 +0100

    First commit from Chapter 04

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Debugging
+{
+    class Program
+    {
+        static double Add(double a, double b) 
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            double a = 4.5;
+            double b = 2.5;
+            double answer = Add(a,b);
+dotnet add package SharpPad            WriteLine($"{a} + {b} = {answer}");
+            ReadLine();    // wait for user to press Enter
+        }
+    }
+}
---
+++ Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Instrumenting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Debug says - I am watching!");
+            Trace.WriteLine("Trace says - I am watching!");        
+        }
+    }
+}
