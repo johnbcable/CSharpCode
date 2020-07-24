2dd7060ad59c6e8a413f2cea2931a0375a8211be
John Cable <JohnCable@Warwickshire.gov.uk>
2020-07-24 13:44:53 +0100

    Removed .mp4 file and started Chapter05

---
+++ Chapter05/PacktLibrary/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Packt.Shared
+{
+    public class Person : object
+    {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+    }
+}
---
+++ Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Packt.Shared;
+using System;
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bob = new Person();
+            bob.Name = "Bob Smith";
+            bob.DateOfBirth = new DateTime(1965, 12, 22);
+
+            WriteLine(
+                format: "{0} was born on {1:dddd, d MMMM yyyy}",
+                arg0: bob.Name,
+                arg1: bob.DateOfBirth);
+
+            var alice = new Person 
+            {
+                Name = "Alice Jones",
+                DateOfBirth = new DateTime(1998, 3, 7)
+            };
+
+            WriteLine(
+                format: "{0} was born on {1:dd MMM yy}",
+                arg0: alice.Name,
+                arg1: alice.DateOfBirth);
+
+        }
+    }
+}
