463080b16e70ca499d8ce202460929e2157d9831
John Cable <JohnCable@Warwickshire.gov.uk>
2020-08-04 09:00:20 +0100

    Added skeleton of Chapter 6 code

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+
+        // methods
+        public void WriteToConsole() 
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+    }
+}
