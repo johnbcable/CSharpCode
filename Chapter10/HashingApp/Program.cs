d5aaeea2a6f8f84388e48cf93038ccde6826f8d9
John Cable <JohnCable@Warwickshire.gov.uk>
2020-09-13 11:46:24 +0100

    First part of Chapter 10 up to and including SigningApp

---
+++ Chapter10/HashingApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Packt.Shared;
+using static System.Console;
+
+namespace HashingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Registering Alice with Pa$$w0rd.");
+            var alice = Protector.Register("Alice", "Pa$$w0rd");
+            WriteLine($"Name:  {alice.Name}");
+            WriteLine($"Salt:  {alice.Salt}");
+            WriteLine("Password (salted and hashed):  {0}",
+            arg0: alice.SaltedHashedPassword);
+            WriteLine();
+
+            Write("Enter a new User to register: ");
+            string username = ReadLine();
+            Write($"Enter a password for {username}: ");
+            string password = ReadLine();
+            var user = Protector.Register(username, password);
+            WriteLine($"Name:  {user.Name}");
+            WriteLine($"Salt:  {user.Salt}");
+            WriteLine("Password (salted and hashed):  {0}",
+            arg0: user.SaltedHashedPassword);
+            WriteLine();
+
+            bool correctPassword = false;
+            while(!correctPassword)
+            {
+                Write("Enter a username to log in: ");
+                string loginUserName = ReadLine();
+                Write($"Enter a password for {loginUserName} to log in: ");
+                string loginPassword = ReadLine();
+
+                correctPassword = Protector.CheckPassword(loginUserName, loginPassword);
+
+                if (correctPassword)
+                {
+                    WriteLine($"Correct! {loginUserName} has been logged in.");
+                }
+                else 
+                {
+                    WriteLine("Invalid username or password. Try again.");
+                }
+
+            }
+
+        }
+    }
+}
